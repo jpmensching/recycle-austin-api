f20a6ebb3cd8381c8caff715b2756ef775c03b20
J.P. Mensching <jmensching@netspend.com>
2020-07-13 14:42:20 -0500

    Create users model.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import Express from 'express';
+import { Sequelize, DataTypes } from 'sequelize';
+import { User } from './models/user';
+
+initDb();
 
 const app = Express();
 
@@ -8,3 +12,18 @@ app.get('/', (req, res) => {
 
 app.listen(3000);
 console.log('App started on port 3000.');
+
+async function initDb() {
+  const sequelize = new Sequelize('sqlite::memory:');
+  User.init({
+    username: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, { sequelize, modelName: 'user' });
+
+  await sequelize.sync();
+  const jane = await User.create({
+    username: 'Jane',
+    password: 'password'
+  });
+  console.log(jane.toJSON());
+}
