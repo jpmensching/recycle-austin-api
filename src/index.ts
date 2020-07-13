ece5fa5daa223b3e0fb958b7dec2180f9728722e
J.P. Mensching <jmensching@netspend.com>
2020-07-13 10:29:58 -0500

    Initial server setup.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Starting Recycle Austin API.');
+import Express from 'express';
+
+const app = Express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Recycle Austin!');
+});
+
+app.listen(3000);
+console.log('App started on port 3000.');
